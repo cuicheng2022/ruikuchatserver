7fabd4835f30d9e13a55641e05a1a781026b4fa6
cuicheng2022 <cuicheng2022@gmail.com>
2021-12-25 01:13:15 +0800

    first commit

--- peer-server/server.js
+++ peer-server/server.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv').config();
 // const express = require("express");
 // const { ExpressPeerServer } = require("peer");
 
@@ -17,7 +17,6 @@ require('dotenv').config()
 // app.use("/peerjs", peerServer);
 
 // server.listen(process.env.PORT || 9000);
-
 
 // const express = require('express');
 // const { ExpressPeerServer } = require('peer');
@@ -57,7 +56,6 @@ require('dotenv').config()
 
 // server.listen(9000);
 
-
 // // initialize express
 // var express = require('express');
 // var app = express();
@@ -75,7 +73,6 @@ require('dotenv').config()
 // app.use('/peerjs', ExpressPeerServer(server, options));
 // // Now listen to your ip and port.
 // server.listen(8878, "http://18.168.149.207/");
-
 
 // const express = require("express");
 // const http = require('http');
@@ -105,12 +102,6 @@ require('dotenv').config()
 // server.listen(port);
 // console.log('Listening on: ' + port);
 
-
-
-
-
-
-
 // var fs = require('fs');
 // var PeerServer = require('peer').PeerServer;
 
@@ -123,13 +114,10 @@ require('dotenv').config()
 //     }
 // });
 
-
-
-
-const express = require("express");
-const http = require("http");
-const socketio = require("socket.io");
-const { ExpressPeerServer } = require("peer");
+const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
+const { ExpressPeerServer } = require('peer');
 // const mongoose = require("mongoose");
 // const config = require("config");
 
@@ -139,22 +127,21 @@ const server = http.createServer(app);
 const io = socketio(server).sockets;
 
 //** Peer Server */
-const customGenerationFunction = () =>
-  (Math.random().toString(36) + "0000000000000000000").substr(2, 16);
+const customGenerationFunction = () => (Math.random().toString(36) + '0000000000000000000').substr(2, 16);
 
 const peerServer = ExpressPeerServer(server, {
   debug: true,
-  path: "/",
+  path: '/',
   generateClientId: customGenerationFunction,
 });
 
-app.use("/mypeer", peerServer);
+app.use('/mypeer', peerServer);
 
 //** Config */
 // const db = config.get("mongoURI");
 
 //* Websocket *//
-io.on("connection", function (socket) {});
+io.on('connection', function (socket) {});
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`Server started on port ${port}`));
--- socket-server/app.js
+++ socket-server/app.js
@@ -35,89 +35,51 @@
 
 // module.exports = app;
 
-const express = require("express");
+const express = require('express');
 const app = express();
-const port = 8001;
-var server = require("http").Server(app);
-const io = require("socket.io")(server);
-const cors = require("cors");
-
-
+const port = 4999;
+var server = require('http').Server(app);
+const io = require('socket.io')(server);
+const cors = require('cors');
 
 app.use(cors());
 
-
-
-
-
-
 io.users = {};
 
-
-io.on("connection", function(socket) {
-
-
- 
-
-
-
-  socket.on("register", (username) => onRegister(socket, username));
-      socket.on("set-peer-id", (peerId) => onSetPeerId(socket, peerId));
-      socket.on("call", (username) => onCall(socket, username));
-      socket.on("reject-call", (username) =>
-        onRejectCall(socket, username)
-      );
-      socket.on("accept-call", (username) =>
-        onAcceptCall(socket, username)
-      );
-      console.log(`${Date(Date.now()).toLocaleString()}: new user connected`);
-      socket.on("disconnect", () => onDisconnect(socket));
-
-
-
-
- 
-
-
-
-
-
-
+io.on('connection', function (socket) {
+  socket.on('register', username => onRegister(socket, username));
+  socket.on('set-peer-id', peerId => onSetPeerId(socket, peerId));
+  socket.on('call', username => onCall(socket, username));
+  socket.on('reject-call', username => onRejectCall(socket, username));
+  socket.on('accept-call', username => onAcceptCall(socket, username));
+  console.log(`${Date(Date.now()).toLocaleString()}: new user connected`);
+  socket.on('disconnect', () => onDisconnect(socket));
 });
 
-
-
-
 onAcceptCall = (socket, username) => {
-  if (io.users[username])
-    io
-      .to(io.users[username].socketId)
-      .emit("accepted-call", io.users[socket.username]);
+  if (io.users[username]) io.to(io.users[username].socketId).emit('accepted-call', io.users[socket.username]);
 };
 
 onRejectCall = (socket, username) => {
   if (io.users[username]) {
-    io
-      .to(io.users[username].socketId)
-      .emit("rejected-call", io.users[socket.username]);
+    io.to(io.users[username].socketId).emit('rejected-call', io.users[socket.username]);
   }
 };
 
 onCall = (socket, username) => {
   if (io.users[username]) {
-    io.to(io.users[username].socketId)
-      .emit("call", io.users[socket.username]);
+    io.to(io.users[username].socketId).emit('call', io.users[socket.username]);
   } else {
-    socket.emit("not-available", username);
+    socket.emit('not-available', username);
   }
 };
 
 onRegister = (socket, username) => {
-  console.log("Registered", username);
+  console.log('Registered', username);
   socket.username = username;
   io.users[username] = {
     username,
-    peerId: "",
+    peerId: '',
     socketId: socket.id,
   };
   onUsersChange(socket);
@@ -125,18 +87,18 @@ onRegister = (socket, username) => {
 
 getUsers = () => {
   const users = [];
-  Object.keys(io.users).forEach((key) => {
+  Object.keys(io.users).forEach(key => {
     users.push(io.users[key]);
   });
   return users;
 };
 
-onUsersChange = (socket) => {
-  io.emit("users-change", getUsers());
+onUsersChange = socket => {
+  io.emit('users-change', getUsers());
 };
 
 onSetPeerId = (socket, peerId) => {
-  console.log("Set Peer Id user:", socket.username, " peerId: ", peerId);
+  console.log('Set Peer Id user:', socket.username, ' peerId: ', peerId);
   io.users[socket.username] = {
     peerId,
     socketId: socket.id,
@@ -145,13 +107,9 @@ onSetPeerId = (socket, peerId) => {
   onUsersChange();
 };
 
-onDisconnect = (socket) => {
+onDisconnect = socket => {
   delete io.users[socket.username];
-  console.log(
-    `${Date(Date.now()).toLocaleString()} ID:${
-      socket.username
-    } user disconnected`
-  );
+  console.log(`${Date(Date.now()).toLocaleString()} ID:${socket.username} user disconnected`);
   onUsersChange();
 };
 
@@ -161,14 +119,6 @@ emit = (event, userId, data) => {
   }
 };
 
+module.exports = app;
 
-
-
-server.listen(port, () =>
-  console.log(`Example app listening on port ${port}!`)
-);
-
-
-
-
-
+server.listen(port, () => console.log(`Example app listening on port ${port}!`));
